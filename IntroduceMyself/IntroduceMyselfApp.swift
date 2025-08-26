ce46c61b52c4819deb2f2eb932396e37b7cc59ac
amelialexandra <amelialexandra.it@gmail.com>
2025-08-27 06:14:16 +0700

    Initial Commit

---
+++ IntroduceMyself/IntroduceMyselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroduceMyselfApp.swift
+//  IntroduceMyself
+//
+//  Created by Amelia Alexandra on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct IntroduceMyselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
