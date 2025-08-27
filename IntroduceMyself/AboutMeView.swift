ae75f3dba7791a03825a598b804c67c264f177ac
amelialexandra <amelialexandra.it@gmail.com>
2025-08-27 07:23:16 +0700

    add all project

---
+++ IntroduceMyself/AboutMeView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMeView.swift
+//  IntroduceMyself
+//
+//  Created by Amelia Alexandra on 27/08/25.
+//
+
+import SwiftUI
+
+struct AboutMeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutMeView()
+}
---
+++ IntroduceMyself/MyProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProfileView.swift
+//  IntroduceMyself
+//
+//  Created by Amelia Alexandra on 27/08/25.
+//
+
+import SwiftUI
+
+struct MyProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProfileView()
+}
